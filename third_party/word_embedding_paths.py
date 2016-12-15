adc8a49792fb58ae8056a8ddef5bcdde2c302626
Jimmy Callin <jimmy.callin@gmail.com>
2016-12-15 14:46:13 +0100

    added word_embedding_paths

---
+++ third_party/word_embedding_paths.py
@@ -0,0 +1,10 @@
+word_embeddings = [{"name": "cbow-googlenews", "path": "/word_embeddings/precompiled/cbow/GoogleNews-vectors-negative300.txt", "size": 300},
+				   {"name": "cbow-wikipedia", "path": "/word_embeddings/precompiled/cbow/size=50.embeddings", "size": 50},
+				   {"name": "cw-wikipedia", "path": "/word_embeddings/precompiled/c&w/size=50.merged", "size": 50},
+				   {"name": "glove-wikipedia", "path": "/word_embeddings/precompiled/glove/size=50.embeddings", "size": 50},
+				   {"name": "glove-wikipedia-gigawords", "path": "/word_embeddings/precompiled/glove/glove.6B.50d.txt", "size": 50},
+				   {"name": "randomprojection-wikipedia", "path": "/word_embeddings/precompiled/random_projection/size=50.merged", "size": 50},
+				   {"name": "hpca-wikipedia", "path": "/word_embeddings/precompiled/hpca/size=50.merged", "size": 50},
+				   {"name": "tscca-wikipedia", "path": "/word_embeddings/precompiled/tscca/size=50.embeddings", "size": 50},
+				   {"name": "rsv-wikipedia", "path": "/word_embeddings/precompiled/rsv/wiki_2008_d100_w1_ncntx10000.wembed", "size": 100},
+				   {"name": "random-vectors", "path": "/word_embeddings/precompiled/random_vectors/random_vectors.50d.txt", "size": 50}]
